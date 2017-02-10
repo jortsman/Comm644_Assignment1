cc0c07b773b3672354618ac15cfcb91e28892135
Vlad Pasculescu <vpasculescu@Vlads-MacBook-Pro.local>
2017-02-09 19:28:29 -0800

    initial commit
    Comm644 Assignment 1 initial submission

---
+++ app.js
@@ -0,0 +1,182 @@
+//Step 1
+/*
+var someMonth;
+function theMonth;
+curentMonth;
+var summerMonth;
+mylibraryAFunction;
+*/
+
+//Step 2
+/*
+23; //numeric literal
+'jortsman'; //string literal
+true; //boolean literal
+null; //null literal
+*/
+
+//Step 3
+/*
+var numberMathCalc = Math.PI*36;
+var varAdd = var1 + " " + var2;
+*/
+
+//Step 4
+/*
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var intZipCode;
+var intYourAge;
+var strReferralSource;
+var blnMayWeContactYou;
+*/
+
+//Step 5
+/*
+var strFirstName = "Vlad";
+blnMayWeContactYou = true;
+var strCity = 'La Jolla', var strState = "California";
+*/
+
+//Step 6
+/*
+var numPlusString = 23 + 'hello world'; //number plus string
+console.log(numPlusString);
+
+var blnPlusString = true + 'Stay classy, San Diego'; //boolean plus string
+console.log(blnPlusString);
+
+var numPlusBoolean = 23 + true; //number plus boolean
+console.log(numPlusBoolean);
+*/
+
+//Step 7
+/*
+stuff = undefined;
+var stuff;
+console.log(stuff);
+*/
+
+//Step 8 - String is not valid beause of escape sequences. Fixed code is below
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);
+*/
+
+//Step 9
+/*
+var valNull = null;
+console.log(valNull);
+var valUndefined = undefined;
+console.log(valUndefined);
+*/
+
+//Step 10
+/*
+var strString = "This is a string";
+console.log("\"" + strString + "\"" + " is a " + typeof strString);
+var numNumber = 23;
+console.log("\"" + numNumber + "\"" + " is a " + typeof numNumber);
+var blnBoolean = true;
+console.log("\"" + blnBoolean + "\"" + " is a " + typeof blnBoolean);
+var objObject = new Date();
+console.log("\"" + objObject + "\"" + " is an " + typeof objObject);
+var undefUndefined;
+console.log("\"" + undefUndefined + "\"" + " is an " + typeof undefUndefined);
+*/
+
+//Step 11
+/*
+window.alert("Hello " + "Zak Ruvalcaba" + "," + " welcome to the JavaScript class!");
+*/
+
+//Step 12
+/*
+var name = "Vlad Pasculescu";
+window.alert("Hello " + name + "," + " welcome to the JavaScript class!");
+*/
+
+//Step 13
+/*
+var name = "Vlad Pasculescu";
+var course = "JavaScript";
+window.alert("Hello " + name + "," + " welcome to the " + course + " class!");
+*/
+
+//Step 14
+/*
+window.alert("Hello Vlad Pasculescu.\nWelcome to the JavaScript class!");
+*/
+
+//Step 15
+/*
+var name = "Vlad Pasculescu";
+window.alert("Hello " + name + "." + "\nWelcome to the JavaScript class!");
+*/
+
+//Step 16
+/*
+var name = "Vlad Pasculescu";
+var course = "JavaScript";
+window.alert("Hello " + name + "." + "\nWelcome to the " + course + " class!");
+*/
+
+//Step 17
+/*
+var x = 10;
+var y = 20;
+console.log(x + y);
+*/
+
+//Step 18
+/*
+var x = 20;
+console.log(x += 20);
+*/
+
+//Step 19
+/*
+var x = 20;
+console.log(x*=5);
+*/
+
+//Step 20
+/*
+var x = 20%3;
+console.log(x/=1);
+*/
+
+//Step 21
+/*
+var x = 2;
+var y = 4;
+console.log(x != y && x + y < 10);
+*/
+
+//Step 22
+/*
+var x = 1;
+var y = "It\'s called \"Sex Panther\", it\'s illegal in 10 countries.";
+console.log(x === y || x > 1);
+*/
+
+//Step 23
+/*
+var widget = new Object();
+console.log(typeof widget);
+*/
+
+//Step 24
+/*
+var widget = new Object();
+console.log(widget instanceof Object);
+*/
+
+//Step 25
+/*
+var widget = new Object();
+console.log(widget instanceof String);
+*/
